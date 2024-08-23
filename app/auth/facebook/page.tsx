d5613974bdcadc500a2dbc801cbda6c86fd07755
Shinyuy Marcel <shinyuy9@gmail.com>
2024-08-23 19:53:14 +0100

    Fix

--- app/auth/facebook/page.tsx
+++ app/auth/facebook/page.tsx
@@ -3,14 +3,18 @@
 import { useSocialAuthenticateMutation } from '../../../redux/features/authApiSlice';
 import { useSocialAuth } from '../../../hooks';
 import { Spinner } from '../../../components/common';
+import { Suspense } from 'react'
 
 export default function Page() {
 	const [facebookAuthenticate] = useSocialAuthenticateMutation();
 	useSocialAuth(facebookAuthenticate, 'facebook');
 
 	return (
-		<div className='my-8'>
-			<Spinner lg />
-		</div>
+		<Suspense>
+			<div className='my-8'>
+				<Spinner lg />
+			</div>
+		</Suspense>
+
 	);
 }
--- app/auth/google/page.tsx
+++ app/auth/google/page.tsx
@@ -3,14 +3,18 @@
 import { useSocialAuthenticateMutation } from '../../../redux/features/authApiSlice';
 import { useSocialAuth } from '../../../hooks';
 import { Spinner } from '../../../components/common';
+import { Suspense } from 'react'
 
 export default function Page() {
 	const [googleAuthenticate] = useSocialAuthenticateMutation();
 	useSocialAuth(googleAuthenticate, 'google-oauth2');
 
 	return (
-		<div className='my-8'>
-			<Spinner lg />
-		</div>
+		<Suspense>
+			<div className='my-8'>
+				<Spinner lg />
+			</div>
+		</Suspense>
+
 	);
 }
