89037c9ac1e5436f01e6d53d28b4075c85406e32
Shinyuy Marcel <shinyuy9@gmail.com>
2024-08-23 18:53:16 +0100

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-"use client";
+
 import './globals.css';
 import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
@@ -9,29 +9,29 @@ import { useEffect, useState } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
-// export const metadata: Metadata = {
-// 	title: 'Bot AI',
-// 	description: 'Unlock the power of AI with our intelligent chatbots, designed to instantly answer questions from your PDFs or internal data sources. Simplify information access, enhance user engagement, and make data-driven decisions effortlessly. Whether it’s legal documents, business reports, or educational material, our chatbots deliver accurate answers in real-time, tailored to your needs. Experience the future of smart communication today'
-// };
+export const metadata: Metadata = {
+	title: 'Bot AI',
+	description: 'Unlock the power of AI with our intelligent chatbots, designed to instantly answer questions from your PDFs or internal data sources. Simplify information access, enhance user engagement, and make data-driven decisions effortlessly. Whether it’s legal documents, business reports, or educational material, our chatbots deliver accurate answers in real-time, tailored to your needs. Experience the future of smart communication today'
+};
 
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const [isClient, setIsClient] = useState(false)
+	// const [isClient, setIsClient] = useState(false)
 
-	useEffect(() => {
-		setIsClient(true)
-	}, [])
+	// useEffect(() => {
+	// 	setIsClient(true)
+	// }, [])
 
 	return (
 		<html lang='en'>
-			<link rel="stylesheet" href="https://bot-client-2b4.pages.dev/style.css" />
+			{/* <link rel="stylesheet" href="https://bot-client-2b4.pages.dev/style.css" />
 			<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-			<script src="https://bot-client-2b4.pages.dev/script.js" defer></script>
+			<script src="https://bot-client-2b4.pages.dev/script.js" defer></script> */}
 
-			<meta name='Unlock the power of AI with our intelligent chatbots, designed to instantly answer questions from your PDFs or internal data sources. Simplify information access, enhance user engagement, and make data-driven decisions effortlessly. Whether it’s legal documents, business reports, or educational material, our chatbots deliver accurate answers in real-time, tailored to your needs. Experience the future of smart communication today' content='Description' />
+			{/* <meta name='Unlock the power of AI with our intelligent chatbots, designed to instantly answer questions from your PDFs or internal data sources. Simplify information access, enhance user engagement, and make data-driven decisions effortlessly. Whether it’s legal documents, business reports, or educational material, our chatbots deliver accurate answers in real-time, tailored to your needs. Experience the future of smart communication today' content='Description' /> */}
 
 
 			<title>Bot AI</title>
@@ -44,8 +44,8 @@ export default function RootLayout({
 					</div>
 					<Footer />
 				</Provider>
-				{isClient &&
-					<script src="https://bot-client-2b4.pages.dev/ui.js"></script>}
+				{/* {isClient &&
+					<script src="https://bot-client-2b4.pages.dev/ui.js"></script>} */}
 			</body>
 		</html>
 	);
