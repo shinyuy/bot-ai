e3c46386eaea870a04d83e0dcda647a10b3b70de
Shinyuy Marcel <shinyuy9@gmail.com>
2024-08-26 10:06:02 +0100

    Fix

--- components/forms/Input.tsx
+++ components/forms/Input.tsx
@@ -43,7 +43,7 @@ export default function Input({
       <div className="mt-2">
         <input
           id={labelId}
-          className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+          className="block w-full border-gray-400 rounded-md border px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
           name={labelId}
           type={type}
           onChange={onChange}
