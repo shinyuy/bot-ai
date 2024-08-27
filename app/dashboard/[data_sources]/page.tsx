930346114d9be6b4abc8d7338994cb9693952634
Shinyuy Marcel <shinyuy9@gmail.com>
2024-08-27 23:53:39 +0100

    Fix

--- app/dashboard/[data_sources]/page.tsx
+++ app/dashboard/[data_sources]/page.tsx
@@ -16,7 +16,7 @@ function getStepContent(step: number, setActiveStep) {
         case 2:
             return <DataSource />;
         case 3:
-            return <FileUpload step={step} setActiveStep={setActiveStep} />;
+            return <FileUpload setActiveStep={setActiveStep} />;
         case 4:
             return <Success />;
         case 5:
