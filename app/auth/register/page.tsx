95429b5456e25d4a1625b28dafb6eebeedd3ee6f
Shinyuy Marcel <shinyuy9@gmail.com>
2024-11-15 07:17:10 +0100

    fix

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -5,7 +5,7 @@ import type { Metadata } from 'next';
 import { Navbar } from '../../../components/common';
 
 export const metadata: Metadata = {
-  title: 'Contexx AI | Register',
+  title: 'Contexx AI | Register Page',
   description: '',
 };
 
